71019298132faf180e9f715c55ee4fddd9b357ec
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2012-08-26 14:02:07 +0530

    Create a CLI parser

---
+++ devlog.py
@@ -0,0 +1,24 @@
+import sys
+import utils
+import optparse 
+from publish import *
+
+def parse_args(options):
+    parser = optparse.OptionParser()
+    parser.add_option('-s','--create-skeleton', action="store_true", default=False)
+    parser.add_option('-c','--create-cronjob', action="store_true", default=False)
+    parser.add_option('-p','--publish', action="store_true", default=False)
+    options, rem = parser.parse_args(options)
+    return options
+
+def create_skeleton():
+    newpath = 'site/posts'
+    if not os.path.exists(newpath): os.makedirs(newpath)
+    
+if __name__ == '__main__':
+    options = parse_args(sys.argv[1:])
+
+    if options.create_skeleton:
+        create_skeleton()
+    if options.publish:
+        publish()
--- publish.py
+++ publish.py
@@ -8,13 +8,13 @@ from mako.template import Template
 from mako.lookup import TemplateLookup
 
 class Post():
-    def __init__(self, title=None, timestamp=None, msg=None, link=None):
+    def __init__(self, title=None, author=None, timestamp=None, msg=None, link=None):
         self.title = title
-        self.author = config['name']
+        self.author = author
         self.timestamp = timestamp
         self.msg = msg
+        self.link = link
         self.filename = self.create_filename(self.title)
-        self.link = config['base url']+self.filename
 
     def create_filename(self, title):
         # replace all non-word chars with '-'
@@ -22,7 +22,10 @@ class Post():
         # replace multiple '-' with single '-', use only first 30 chars
         return re.sub(r'-+', '-', link).strip('-')[:30]+".html"
 
-def publish():    
+def publish():
+    # parse the config file
+    config = utils.parse_config("config.ini")
+    
     # store all posts
     posts = []
 
@@ -33,23 +36,24 @@ def publish():
     for root, dir, files in os.walk(path):
         for file in files:
             with open(os.path.join(root, file)) as fh:
-                post = parse_mail(fh)
+                post = parse_mail(fh, config)
                 if post:
-                    make_post(post)
+                    make_post(post, config)
                     posts.append(post)
 
-    make_index(posts)
+    make_index(posts, config)
 
-def parse_mail(mail):
+def parse_mail(mail, config):
     mail = Parser().parse(mail)
 
     # this works for me, might be break for stupid^Wweirdly configured clients
     sender = mail['from'].split('<')[0].strip()
 
-    if config['name'] != sender:
+    if config['author'] != sender:
         return
 
-    post = Post(mail['subject'], mail['date'])
+    post = Post(mail['subject'], config['author'], mail['date'])
+    post.link = config['base url']+post.filename
 
     for part in mail.walk():
         # i support only plain text, sending stupid^Wfancy html mails wont work
@@ -62,33 +66,27 @@ def parse_mail(mail):
 
     return post
 
-def make_post(post):
+def make_post(post, config):
     # path to templates
     template_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
     template_path = os.path.join(template_path, 'base.html')
     mytemplate = Template(filename=template_path, module_directory='/tmp/mako_modules')
     
     #path to output file
-    outfh_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'posts')
+    outfh_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'site/posts')
     outfh_path = os.path.join(outfh_path, post.filename)
     
     with open(outfh_path, 'w') as outfh:
-        outfh.write(mytemplate.render(post=post))
+        outfh.write(mytemplate.render(post=post, config=config))
 
-def make_index(posts):
+def make_index(posts, config):
     # path to templates
     template_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
     template_path = os.path.join(template_path, 'index.html')
     mytemplate = Template(filename=template_path, module_directory='/tmp/mako_modules')
     
     #path to output file
-    outfh_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'index.html')
-    
+    outfh_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'site/index.html')
+
     with open(outfh_path, 'w') as outfh:
-        outfh.write(mytemplate.render(posts=posts))
-
-if __name__ == '__main__':
-    # parse the config file
-    config = utils.parse_config("config.ini")
-
-    publish()
+        outfh.write(mytemplate.render(posts=posts, config=config))
